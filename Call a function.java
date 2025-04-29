b6ad025b172f936287662da6fe0de71c82ebefc9
Brian Kipkoech <brian.bwogo@strathmore.edu>
2025-04-29 12:59:21 +0300

    Create Call a function.java

---
+++ Call a function.java
@@ -0,0 +1,12 @@
+public class Main {
+    // Define the function
+    public static int square(int number) {
+        return number * number;
+    }
+
+    // Main method to call the function
+    public static void main(String[] args) {
+        int result = square(5);
+        System.out.println("The square is: " + result);
+    }
+}
