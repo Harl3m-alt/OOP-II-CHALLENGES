5b5c2563efd5bbf733f37506efe70a59783d9500
Brian Kipkoech <brian.bwogo@strathmore.edu>
2025-04-29 13:13:57 +0300

    Create Create an 'if', 'else' code.java

---
+++ Create an 'if', 'else' code.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare a variable
+        int number = -3;
+
+        // Use if-else to check the value
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
